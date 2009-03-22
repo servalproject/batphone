47c63829f48bb5979d31784172571c14ae79d4e2
harald.mue <harald.mue@58ca5e2c-0207-11de-85c2-bb0660c75f22>
2009-03-22 12:56:52 +0000

    License upgrade to GPLv3.
    git-svn-id: http://android-wifi-tether.googlecode.com/svn/trunk@75 58ca5e2c-0207-11de-85c2-bb0660c75f22

--- src/android/tether/system/CoreTask.java
+++ src/android/tether/system/CoreTask.java
@@ -1,12 +1,13 @@
 /**
- *  This software is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
+ *  This program is free software; you can redistribute it and/or modify it under 
+ *  the terms of the GNU General Public License as published by the Free Software 
+ *  Foundation; either version 3 of the License, or (at your option) any later 
+ *  version.
+ *  You should have received a copy of the GNU General Public License along with 
+ *  this program; if not, see <http://www.gnu.org/licenses/>. 
  *  Use this application at your own risk.
- *  
- *  Copyright (c) 2009 by Harald Mueller.
+ *
+ *  Copyright (c) 2009 by Harald Mueller and Seth Lemons.
  */
 
 package android.tether.system;
