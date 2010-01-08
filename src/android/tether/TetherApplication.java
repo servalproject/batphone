9508f0a6a96e82575c214079912698109a4310f3
bbuxton <bbuxton@58ca5e2c-0207-11de-85c2-bb0660c75f22>
2010-01-08 06:14:54 +0000

    Nexus One tether fix.
    git-svn-id: http://android-wifi-tether.googlecode.com/svn/trunk@324 58ca5e2c-0207-11de-85c2-bb0660c75f22

--- src/android/tether/TetherApplication.java
+++ src/android/tether/TetherApplication.java
@@ -117,7 +117,7 @@ public class TetherApplication extends Application {
 	public WebserviceTask webserviceTask = null;
 	
 	// Update Url
-	private static final String APPLICATION_PROPERTIES_URL = "http://android-wifi-tether.googlecode.com/svn/download/update/htc/all/application.properties";
+	private static final String APPLICATION_PROPERTIES_URL = "http://android-wifi-tether.googlecode.com/svn/download/update/google/nexusone/application.properties";
 	private static final String APPLICATION_DOWNLOAD_URL = "http://android-wifi-tether.googlecode.com/files/";
 	
 	@Override
