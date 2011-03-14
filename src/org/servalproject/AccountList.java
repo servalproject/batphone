8183a0aa224893c8f14d80b94fc03981fb803378
Jeremy Lakeman <Jeremy.Lakeman@gmail.com>
2011-03-15 09:36:19 +1030

    Add new serval batphone icon

---
+++ src/org/servalproject/AccountList.java
@@ -0,0 +1,14 @@
+package org.servalproject;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+public class AccountList extends ListActivity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+    	super.onCreate(savedInstanceState);
+    	setContentView(R.layout.accountlist);
+    	
+    }
+}
