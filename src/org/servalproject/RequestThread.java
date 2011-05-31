35e9a56434c8275bc939dffb7f41734f75a0b56c
Jeremy Lakeman <Jeremy.Lakeman@gmail.com>
2011-05-31 10:24:11 +0930

    Allow simlinked apk's to be downloaded

--- src/org/servalproject/RequestThread.java
+++ src/org/servalproject/RequestThread.java
@@ -1,4 +1,4 @@
-/* 
+/*
 Copyright Paul James Mutton, 2001-2004, http://www.jibble.org/
 
 This file is part of Mini Wegb Server / SimpleWebServer.
@@ -16,9 +16,17 @@ $Id: ServerSideScriptEngine.java,v 1.4 2004/02/01 13:37:35 pjm2 Exp $
 
 package org.servalproject;
 
-import java.io.*;
-import java.net.*;
-import java.util.*;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.URLDecoder;
+import java.util.Date;
 
 /**
  * Copyright Paul Mutton
@@ -26,14 +34,14 @@ import java.util.*;
  *
  */
 public class RequestThread extends Thread {
-    
+
     public RequestThread(Socket socket, File rootDir) {
         _socket = socket;
         _rootDir = rootDir;
     }
-    
+
     private static void sendHeader(BufferedOutputStream out, int code, String contentType, long contentLength, long lastModified) throws IOException {
-        out.write(("HTTP/1.0 " + code + " OK\r\n" + 
+        out.write(("HTTP/1.0 " + code + " OK\r\n" +
                    "Date: " + new Date().toString() + "\r\n" +
                    "Server: JibbleWebServer/1.0\r\n" +
                    "Content-Type: " + contentType + "\r\n" +
@@ -42,7 +50,7 @@ public class RequestThread extends Thread {
                    "Last-modified: " + new Date(lastModified).toString() + "\r\n" +
                    "\r\n").getBytes());
     }
-    
+
     private static void sendError(BufferedOutputStream out, int code, String message) throws IOException {
         message = message + "<hr>" + SimpleWebServer.VERSION;
         sendHeader(out, code, "text/html", message.length(), System.currentTimeMillis());
@@ -50,23 +58,24 @@ public class RequestThread extends Thread {
         out.flush();
         out.close();
     }
-    
-    public void run() {
+
+    @Override
+	public void run() {
         InputStream reader = null;
         try {
             _socket.setSoTimeout(30000);
             BufferedReader in = new BufferedReader(new InputStreamReader(_socket.getInputStream()));
             BufferedOutputStream out = new BufferedOutputStream(_socket.getOutputStream());
-            
+
             String request = in.readLine();
             if (request == null || !request.startsWith("GET ") || !(request.endsWith(" HTTP/1.0") || request.endsWith("HTTP/1.1"))) {
                 // Invalid request type (no "GET")
                 sendError(out, 500, "Invalid Method.");
                 return;
-            }            
-            String path = request.substring(4, request.length() - 9);            
+            }
+            String path = request.substring(4, request.length() - 9);
             File file = new File(_rootDir, URLDecoder.decode(path, "UTF-8")).getCanonicalFile();
-            
+
             if (file.isDirectory()) {
                 // Check to see if there is an index file in the directory.
                 File indexFile = new File(file, "index.html");
@@ -75,12 +84,7 @@ public class RequestThread extends Thread {
                 }
             }
 
-            if (!file.toString().startsWith(_rootDir.toString())) {
-                // Uh-oh, it looks like some lamer is trying to take a peek
-                // outside of our web root directory.
-                sendError(out, 403, "Permission Denied.");
-            }
-            else if (!file.exists()) {
+			if (!file.exists()) {
                 // The file was not found.
                 sendError(out, 404, "File Not Found.");
             }
@@ -106,14 +110,14 @@ public class RequestThread extends Thread {
             }
             else {
                 reader = new BufferedInputStream(new FileInputStream(file));
-            
-                String contentType = (String)SimpleWebServer.MIME_TYPES.get(SimpleWebServer.getExtension(file));
+
+                String contentType = SimpleWebServer.MIME_TYPES.get(SimpleWebServer.getExtension(file));
                 if (contentType == null) {
                     contentType = "application/octet-stream";
                 }
-                
+
                 sendHeader(out, 200, contentType, file.length(), file.lastModified());
-                
+
                 byte[] buffer = new byte[4096];
                 int bytesRead;
                 while ((bytesRead = reader.read(buffer)) != -1) {
@@ -135,8 +139,8 @@ public class RequestThread extends Thread {
             }
         }
     }
-    
+
     private File _rootDir;
     private Socket _socket;
-    
+
 }
