95f37268d23dd31f0576d5bb1c44c5dc49f7ef0b
Jeremy Lakeman <Jeremy.Lakeman@gmail.com>
2011-07-12 12:31:44 +0930

    Change call UI name and try to improve reliability

--- src/org/sipdroid/sipua/ui/SIPUri.java
+++ src/org/sipdroid/sipua/ui/SIPUri.java
@@ -2,19 +2,19 @@
  * Copyright (C) 2009 The Sipdroid Open Source Project
  * Copyright (C) 2009 Nominet UK and contributed to
  * the Sipdroid Open Source Project
- * 
+ *
  * This file is part of Sipdroid (http://www.sipdroid.org)
- * 
+ *
  * Sipdroid is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This source code is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this source code; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -29,6 +29,7 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnCancelListener;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.Window;
@@ -43,6 +44,7 @@ public class SIPUri extends Activity {
 			.setIcon(R.drawable.icon22)
 			.setCancelable(true)
 			.setOnCancelListener(new OnCancelListener() {
+				@Override
 				public void onCancel(DialogInterface dialog) {
 					finish();
 				}
@@ -51,7 +53,7 @@ public class SIPUri extends Activity {
 		} else
 			finish();
 	}
-	
+
 	/* (non-Javadoc)
 	 * @see android.app.Activity#onCreate(android.os.Bundle)
 	 */
@@ -65,7 +67,8 @@ public class SIPUri extends Activity {
 		SipdroidEngine.on(this,true);
 		Uri uri = getIntent().getData();
 		String target;
-		if (uri.getScheme().equals("sip"))
+		if (uri.getScheme().equals("sip")
+				|| uri.getScheme().equals("servaldna"))
 			target = uri.getSchemeSpecificPart();
 		else {
 			if (uri.getAuthority().equals("aim") ||
@@ -81,41 +84,48 @@ public class SIPUri extends Activity {
 		}
 		if (!target.contains("@")) {
 			final String t = target;
-			final String items[] = {getString(R.string.app_name),
+			// TODO strings...
+			final String items[] = { "Mesh Call",
 					getString(R.string.pstn_name)
 			};
-			
+
 			// TODO replace UI with something that updates as gateway information is received.
-			
+
 			new AlertDialog.Builder(this)
 			.setIcon(R.drawable.icon22)
 			.setTitle(target)
-            .setItems(items, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int whichButton) {
-                    	switch (whichButton) {
-                    	case 0:
-                    		call(t);
-                    		break;
-                    	case 1:
-                			PSTN.callPSTN("sip:"+t);
-                			finish();
-                    	}
-                    }
-                })
+					.setItems(items, new DialogInterface.OnClickListener() {
+						@Override
+						public void onClick(DialogInterface dialog,
+								int whichButton) {
+							switch (whichButton) {
+							case 0:
+								call(t);
+								break;
+							case 1:
+								Intent intent = new Intent(Intent.ACTION_CALL,
+										Uri.fromParts("tel", Uri.decode(t)
+												+ "+", null));
+								intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+								Receiver.mContext.startActivity(intent);
+							}
+						}
+					})
 			.setOnCancelListener(new OnCancelListener() {
+				@Override
 				public void onCancel(DialogInterface dialog) {
 					finish();
 				}
 			})
 			.show();
 		} else
-			call(target); 
+			call(target);
 	}
-	
-	    @Override
-	    public void onPause() {
-	        super.onPause();
-	        finish();
-	    }
-	 
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		finish();
+	}
+
 }
