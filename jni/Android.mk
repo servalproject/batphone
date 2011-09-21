8c755b7c0906d8452d5af74a0670a2af32f1b45f
Jeremy Lakeman <Jeremy.Lakeman@gmail.com>
2011-09-21 11:27:59 +0930

    Remove commented out native library build

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,11 +1,5 @@
 LOCAL_PATH:= $(call my-dir)
 
-# include $(CLEAR_VARS)
-# LOCAL_MODULE    := libnativetask
-# LOCAL_SRC_FILES := libnativetask/org_servalproject_system_NativeTask.c
-# LOCAL_LDLIBS := -lc
-# include $(BUILD_SHARED_LIBRARY)
-
 include $(CLEAR_VARS)
 SPEEX	:= speex-1.2rc1
 SILK     := silk
