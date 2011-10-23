1849134c3b146ee0b69da5066b29e6f5d52f5a5c
gardners <paul@servalproject.org>
2011-10-23 18:55:56 +1030

    Added wrapper for NaCl safe random number source.

--- src/to/yp/cr/NaCl.java
+++ src/to/yp/cr/NaCl.java
@@ -3,17 +3,26 @@ package to.yp.cr;
 
 public class NaCl {
 
+	public final static int crypto_box_PUBLICKEYBYTES = 32;
+	public final static int crypto_box_SECRETKEYBYTES = 32;
+	public final static int crypto_box_NONCEBYTES = 24;
+
 	static {
 		System.loadLibrary("dnalib");
 	}
 
+	protected static native int nativeRandomBytes(byte[] b);
 	protected static native int nativeCryptoBoxKeypair(byte[] a, byte[] b);
+	protected static native int nativeCryptoBox(byte[] publicKey,
+			byte[] secretKey, byte[] nonce, byte[] plaintext,
+			int plaintextLength, byte[] cipherText);
+
+	public static void safeRandomBytes(byte[] a) {
+		if (NaCl.nativeRandomBytes(a) != 0)
+			throw new RuntimeException();
+	}
 
 	public static class CryptoBoxKeypair {
-
-
-		public final static int crypto_box_PUBLICKEYBYTES = 32;
-		public final static int crypto_box_SECRETKEYBYTES = 32;
 
 		byte[] publicKey;
 		byte[] secretKey;
@@ -26,4 +35,23 @@ public class NaCl {
 		}
 
 	}
+
+	public static class CryptoBox {
+
+		byte[] nonce;
+		byte[] cipherText;
+		int cipherLength;
+		int result;
+
+		public CryptoBox(byte[] publicKey, byte[] secretKey,
+				byte[] plainText) {
+			nonce = new byte[crypto_box_NONCEBYTES];
+			NaCl.safeRandomBytes(nonce);
+			cipherText = new byte[plainText.length];
+			result = NaCl.nativeCryptoBox(publicKey, secretKey, nonce,
+					plainText, plainText.length, cipherText);
+		}
+
+	}
+
 }
