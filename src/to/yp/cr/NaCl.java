68f11cfd541adb332d4f8797b1438fd468974249
gardners <paul@servalproject.org>
2011-10-23 16:11:41 +1030

    NaCl native wrapping now reasonably sane, with a single class containing the native functions, and separate classes within that to wrap the calls.

--- src/to/yp/cr/NaCl.java
+++ src/to/yp/cr/NaCl.java
@@ -7,23 +7,22 @@ public class NaCl {
 		System.loadLibrary("dnalib");
 	}
 
+	protected static native int nativeCryptoBoxKeypair(byte[] a, byte[] b);
+
 	public static class CryptoBoxKeypair {
-		private native int method(byte[] a, byte[] b);
+
 
 		public final static int crypto_box_PUBLICKEYBYTES = 32;
 		public final static int crypto_box_SECRETKEYBYTES = 32;
 
 		byte[] publicKey;
 		byte[] secretKey;
-
-		static {
-			System.loadLibrary("dnalib");
-		}
+		int result;
 
 		public CryptoBoxKeypair() {
 			publicKey = new byte[crypto_box_PUBLICKEYBYTES];
 			secretKey = new byte[crypto_box_SECRETKEYBYTES];
-			method(secretKey, publicKey);
+			result = NaCl.nativeCryptoBoxKeypair(secretKey, publicKey);
 		}
 
 	}
