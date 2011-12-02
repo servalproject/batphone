7120f8f5ab4b2c4ca1fde4a7e839a52774939147
gardners <paul@servalproject.org>
2011-12-02 21:40:06 +1030

    Fixed wrapper function wrong function. Something is still not right, though, as it doesn't show any output.

--- jni/iwstatuswrap.c
+++ jni/iwstatuswrap.c
@@ -4,8 +4,7 @@
 int main(int argc,char **argv)
 {
  void *h = dlopen("/data/data/org.servalproject/lib/libiwstatus.so",RTLD_LAZY);
- int (*dnamain)(int,char **) = dlsym(h,"main_iwstatus");
- if (!dnamain) return fprintf(stderr,"Could not load libiwstatus.so\n");
- return (*dnamain)(argc,argv);
-
+ int (*themain)(int,char **) = dlsym(h,"main_iwconfig");
+ if (!themain) return fprintf(stderr,"Could not load libiwstatus.so\n");
+ return (*themain)(argc,argv);
 }
