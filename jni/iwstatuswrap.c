a6f5999323cab70e55917bd6d223f399504620da
gardners <paul@servalproject.org>
2011-12-04 08:11:56 +1030

    attempted fixes and debugging for iwconfig jni bridge (looks like it might need root to run reliably, so will probably switch to using iwconfig from the command line instead).

--- jni/iwstatuswrap.c
+++ jni/iwstatuswrap.c
@@ -5,6 +5,10 @@ int main(int argc,char **argv)
 {
  void *h = dlopen("/data/data/org.servalproject/lib/libiwstatus.so",RTLD_LAZY);
  int (*themain)(int,char **) = dlsym(h,"main_iwconfig");
+ char *msg = dlsym(h,"msg_out");
  if (!themain) return fprintf(stderr,"Could not load libiwstatus.so\n");
- return (*themain)(argc,argv);
+ (*themain)(argc,argv);
+
+ printf("completed:\n[%s]\n",msg);
+ return 0;
 }
