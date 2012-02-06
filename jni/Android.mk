a2111965ca6e568b2bfc57cb51d4109c6536e4a0
gardners <paul@servalproject.org>
2012-02-06 17:33:22 +1030

    Corrected batman version.

--- jni/Android.mk
+++ jni/Android.mk
@@ -32,5 +32,32 @@ LOCAL_SRC_FILES:= iwstatuswrap.c
 
 include $(BUILD_EXECUTABLE)
 
+include $(CLEAR_VARS)
+BATMAND	:= batmand
+LOCAL_CFLAGS	:= -Wall -W -Os -g3 -std=gnu99 -DDEBUG_MALLOC -DMEMORY_USAGE -DPROFILE_DATA -DNO_POLICY_ROUTING -DREVISION_VERSION=\"3.2\" -Dfdatasync=fsync
+LOCAL_MODULE	:= batmand
+LOCAL_SRC_FILES := batman/allocate.c \
+		   batman/batman.c \
+		   batman/bitarray.c \
+		   batman/hash.c \
+		   batman/hna.c \
+		   batman/linux/kernel.c \
+		   batman/linux/route.c \
+		   batman/linux/tun.c \
+		   batman/list-batman.c \
+		   batman/originator.c \
+		   batman/posix/init.c \
+		   batman/posix/posix.c \
+		   batman/posix/tunnel.c \
+		   batman/posix/unix_socket.c \
+		   batman/profile.c \
+		   batman/ring_buffer.c \
+		   batman/schedule.c \
+		   batman/dprintf.c
+
+LOCAL_ARM_MODE:= arm
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/batman
+include $(BUILD_EXECUTABLE) 
+
 include $(LOCAL_PATH)/serval-dna/Android.mk
 
