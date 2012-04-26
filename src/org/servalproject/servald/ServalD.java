6516eba2237ada2677d781acf6d1b282fc87e8e3
Andrew Bettison <andrewb@zip.com.au>
2012-04-26 10:57:16 +0930

    Add ServalD.rhizomeList() method, plus exceptions

--- src/org/servalproject/servald/ServalD.java
+++ src/org/servalproject/servald/ServalD.java
@@ -1,7 +1,14 @@
 package org.servalproject.servald;
 
 import org.servalproject.servald.ServalDResult;
+import org.servalproject.servald.ServalDFailureException;
+import org.servalproject.servald.ServalDInterfaceError;
 
+/**
+ * Low-level class for invoking servald JNI command-line operations.
+ *
+ * @author Andrew Bettison <andrew@servalproject.com>
+ */
 class ServalD
 {
 	public ServalD()
@@ -11,13 +18,60 @@ class ServalD
 
 	public native ServalDResult command(String[] args);
 
-	public static void main(String[] args)
+	/**
+	 * Return a list of file manifests currently in the Rhizome store.
+	 *
+	 * @param offset	Ignored if negative, otherwise passed to the SQL SELECT query in the OFFSET
+	 * 					clause.
+	 * @param limit 	Ignored if negative, otherwise passed to the SQL SELECT query in the LIMIT
+	 * 					clause.
+	 * @return			Array of rows, first row contains column labels.  Each row is an array of
+	 * 					strings, all rows have identical array length.
+	 *
+	 * @author Andrew Bettison <andrew@servalproject.com>
+	 */
+	public String[][] rhizomeList(int offset, int limit) throws ServalDFailureException, ServalDInterfaceError
 	{
-		ServalD sdi = new ServalD();
-		ServalDResult res = sdi.command(args);
-		for (String s: res.outv) {
-			System.out.println(s);
+		String[] args;
+		if (limit >= 0) {
+			if (offset < 0)
+				offset = 0;
+			args = new String[2];
+			args[0] = "" + offset;
+			args[1] = "" + limit;
+		} else if (offset > 0) {
+			args = new String[1];
+			args[0] = "" + offset;
+		} else {
+			args = new String[0];
 		}
-		System.exit(res.status);
+		final ServalDResult result = this.command(args);
+		if (result.status != 0) {
+			throw new ServalDFailureException("return status = " + result.status);
+		}
+		try {
+			int i = 0;
+			final int ncol = Integer.decode(result.outv[i++]);
+			assert ncol > 0;
+			final int nrows = (result.outv.length - 1) / ncol - 1;
+			assert nrows >= 0;
+			assert result.outv.length == (nrows + 1) * ncol + 1;
+			String[][] ret = new String[nrows][ncol];
+			int row, col;
+			for (row = 0; row != nrows; ++row)
+				for (col = 0; col != ncol; ++col)
+					ret[row][col] = result.outv[i++];
+			return ret;
+		}
+		catch (IndexOutOfBoundsException e) {
+			throw new ServalDInterfaceError(e);
+		}
+		catch (IllegalArgumentException e) {
+			throw new ServalDInterfaceError(e);
+		}
+		catch (AssertionError e) {
+			throw new ServalDInterfaceError(e);
+		}
 	}
+
 }
