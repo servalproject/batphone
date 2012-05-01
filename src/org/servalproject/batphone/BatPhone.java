86f376f323bbc69c287678421e46387471203de5
gardners <paul@servalproject.org>
2012-05-01 13:51:13 +0930

    Added CallDirector and hooks to intercept outbound phone calls. CallDirector allows making call by cellular, cancelling, or via mesh.  Automatically populates list with any matching SIDs by doing a DNA lookup in the background.

--- src/org/servalproject/batphone/BatPhone.java
+++ src/org/servalproject/batphone/BatPhone.java
@@ -1,8 +1,11 @@
 package org.servalproject.batphone;
 
+import org.servalproject.ServalBatPhoneApplication;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.SystemClock;
 import android.util.Log;
 
 
@@ -26,10 +29,62 @@ public class BatPhone extends BroadcastReceiver {
 
 	}
 
+	static String dialed_number = null;
+	static long dial_time = 0;
+
+	public static String getDialedNumber()
+	{
+		return dialed_number;
+	}
+
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		// TODO Auto-generated method stub
 		Log.d("BatPhoneReceiver", "Got an intent: " + intent.toString());
+		String intentAction = intent.getAction();
+		String number = getResultData();
+		Boolean force = false;
+
+		// Set result data to null if we are claiming the call, else set
+		// the result data to the number so that someone else can take it.
+
+		// Let the system complete the call if we have said we aren't interested
+		// in it.
+		if (dialed_number != null && dialed_number.equals(number)
+				&& (SystemClock.elapsedRealtime() - dial_time) < 3000) {
+			setResultData(number);
+			return;
+		}
+
+		// Remember this calling attempt
+		dialed_number = number;
+		dial_time = SystemClock.elapsedRealtime();
+
+		// Don't let anyone else try to complete the call while we are
+		// giving the user the choice of how to handle it.
+		setResultData(null);
+
+		// Send call to distributor to select how to handle it.
+		Intent myIntent = new Intent(ServalBatPhoneApplication.context,
+				CallDirector.class);
+		// Create call as a standalone activity stack
+		myIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		// Uncomment below if we want to allow multiple mesh calls in progress
+		// myIndent.addFlags(Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+		ServalBatPhoneApplication.context.startActivity(myIntent);
+	}
+
+	public static void ignoreCall(String number) {
+		// Make this call by the normal method
+		// (more correctly, tell us to ignore this number if someone
+		// dials it in the next 3 seconds)
+		dial_time = SystemClock.elapsedRealtime();
+		dialed_number = number;
+	}
+
+	public static void cancelCall() {
+		dial_time = 0;
+		dialed_number = null;
 	}
 
 }
---
+++ src/org/servalproject/batphone/CallDirector.java
@@ -0,0 +1,103 @@
+package org.servalproject.batphone;
+
+import org.servalproject.servald.ServalD;
+import org.servalproject.servald.ServalDResult;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+public class CallDirector extends ListActivity {
+
+	String dialed_number = BatPhone.getDialedNumber();
+	String dids[] = null;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		this.setTitle("How shall I call " + BatPhone.getDialedNumber() + "?");
+		super.onCreate(savedInstanceState);
+
+		searchMesh(BatPhone.getDialedNumber());
+	}
+
+	private void searchMesh(String did) {
+		ListAdapter adapter = createAdapter(true);
+		setListAdapter(adapter);
+
+		new AsyncTask<String, String, String[]>() {
+			@Override
+			protected void onPostExecute(String s[]) {
+				// Runs on IO thread
+				dids = s;
+				ListAdapter adapter = createAdapter(false);
+				setListAdapter(adapter);
+			}
+
+			@Override
+			protected String[] doInBackground(String... params) {
+				String did = BatPhone.getDialedNumber();
+				ServalDResult results = ServalD.command("dna", "lookup", did);
+				return results.outv;
+			}
+
+		}.execute();
+	}
+
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		if (position == 0) {
+			// make call by cellular/normal means
+			BatPhone.ignoreCall(dialed_number);
+			String url = "tel:" + dialed_number;
+			Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse(url));
+			startActivity(intent);
+			finish();
+		} else if (position == 1) {
+			// cancel call
+			BatPhone.cancelCall();
+			finish();
+		} else if (position == 2) {
+			// if not currently probing the mesh, send out another probe
+			Object o = super.getListAdapter().getItem(2);
+			String s = o.toString();
+			if (s.equals("Search on the mesh")) {
+				searchMesh(BatPhone.getDialedNumber());
+			}
+		} else if (position > 2) {
+			// Mesh call using the specified identity
+		}
+		super.onListItemClick(l, v, position, id);
+	}
+
+	protected ListAdapter createAdapter(boolean probingP)
+	{
+		// Dids contains list of SID,DID tuples, so we only use every 2nd.
+		// XXX Soon we will allow storing of names, in which case we will get
+		// SID,DID,Name, and have to divide by 3
+		int did_count=0;
+		if (dids!=null) did_count=dids.length;
+		String[] values = new String[3 + did_count / 2];
+		values[0]="Normal (cellular) call";
+		values[1]="Cancel call";
+		if (probingP) values[2]="Probing the mesh ...";
+		else values[2]="Search on the mesh";
+		if (dids != null)
+			for (int i = 0; i < dids.length; i += 2) {
+				values[3 + i / 2] = dids[i];
+			}
+
+		// Create a simple array adapter (of type string) with the test values
+		ListAdapter adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, values);
+
+		return adapter;
+	}
+
+
+}
