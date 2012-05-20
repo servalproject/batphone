21d99894655de5bc3c18f72fccb28f358e6dc794
Brendon Kelly <brendon.kelly@gmail.com>
2012-05-20 23:43:50 +0930

    improve layout of coversation message list to bubble-fy messages

--- src/org/servalproject/messages/ShowConversationListAdapter.java
+++ src/org/servalproject/messages/ShowConversationListAdapter.java
@@ -1,15 +1,20 @@
 package org.servalproject.messages;
 
+import java.text.DateFormat;
+
 import org.servalproject.R;
 import org.servalproject.provider.MessagesContract;
 import org.servalproject.servald.Identities;
+import org.servalproject.servald.SubscriberId;
 
 import android.content.Context;
 import android.database.Cursor;
-import android.graphics.Bitmap;
 import android.text.format.DateUtils;
+import android.text.format.Time;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ImageView;
+import android.view.ViewGroup;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
@@ -24,9 +29,14 @@ public class ShowConversationListAdapter extends SimpleCursorAdapter {
 	 * private class level variables
 	 */
 	private Context context;
-	// private int layout;
 
-	private String selfPhoneNumber;
+	private SubscriberId selfIdentity;
+
+	private LayoutInflater layoutInflater;
+
+	private int layout;
+
+	private Time t;
 
 	/**
 	 * constructor for the class
@@ -41,87 +51,124 @@ public class ShowConversationListAdapter extends SimpleCursorAdapter {
 			String[] from, int[] to) {
 		super(context, layout, c, from, to);
 
-		this.context = context;
-		// this.layout = layout;
+		layoutInflater = LayoutInflater.from(context);
 
-		this.selfPhoneNumber = Identities.getCurrentDid();
+		this.context = context;
+		this.layout = layout;
+
+		this.selfIdentity = Identities.getCurrentIdentity();
+
+		// get current date
+		t = new Time();
+		t.setToNow();
+
+	}
+
+	@Override
+	public View newView(Context context, Cursor cursor, ViewGroup parent) {
+
+		for (String s : cursor.getColumnNames()) {
+			Log.i(TAG, "column name " + s);
+		}
+
+		View view;
+		TextView messageText;
+		TextView timeText;
+		// check to see if this is a sent or received message
+		String senderSid = cursor.getString(cursor
+				.getColumnIndex(MessagesContract.Table.SENDER_PHONE));
+		if (senderSid != null
+				&& selfIdentity.equals(new SubscriberId(senderSid))) {
+			view = layoutInflater.inflate(R.layout.show_conversation_item_us, parent, false);
+			messageText = (TextView) view
+					.findViewById(R.id.show_conversation_item_content_us);
+			timeText = (TextView) view
+					.findViewById(R.id.show_conversation_item_time_us);
+		} else {
+			view = layoutInflater.inflate(R.layout.show_conversation_item_them, parent, false);
+			messageText = (TextView) view
+					.findViewById(R.id.show_conversation_item_content_them);
+			timeText = (TextView) view
+					.findViewById(R.id.show_conversation_item_time_them);
+		}
+
+		// get the message text
+		messageText.setText(cursor.getString(cursor
+				.getColumnIndex(MessagesContract.Table.MESSAGE)));
+
+		// format the date and time
+		String mDate = (String) DateUtils.formatSameDayTime(
+				cursor.getLong(cursor
+						.getColumnIndex(MessagesContract.Table.RECEIVED_TIME)),
+				t.toMillis(false), DateFormat.MEDIUM, DateFormat.SHORT);
+		timeText.setText(mDate);
+
+		MessageHolder holder = new MessageHolder();
+		holder.messageView = messageText;
+		holder.timeView = timeText;
+		view.setTag(holder);
+
+		return view;
 	}
 
 	@Override
 	public void bindView(View view, Context context, Cursor cursor) {
 
-		TextView mTextView;
+		MessageHolder holder = (MessageHolder) view.getTag();
 
-		ImageView mImageView = (ImageView) view
-				.findViewById(R.id.show_conversation_item_image);
-
-		// check to see if this is a sent or received message
-		if (selfPhoneNumber.equals(cursor.getString(cursor
-				.getColumnIndex(MessagesContract.Table.SENDER_PHONE))) == true) {
-
-			// this is a message sent by the user
-			mTextView = (TextView) view
-					.findViewById(R.id.show_conversation_item_title);
-			mTextView.setText(selfPhoneNumber);
-
-			populatePhoto(mImageView, cursor, selfPhoneNumber);
-
-		} else {
-
-			// this is a message received by the user
-			mTextView = (TextView) view
-					.findViewById(R.id.show_conversation_item_title);
-			mTextView.setText(cursor.getString(cursor
-					.getColumnIndex(MessagesContract.Table.RECIPIENT_PHONE)));
-
-			populatePhoto(mImageView, cursor, cursor.getString(cursor
-					.getColumnIndex(MessagesContract.Table.RECIPIENT_PHONE)));
-		}
-
-		int mFlags = 0;
-		mFlags |= DateUtils.FORMAT_SHOW_DATE;
-		mFlags |= DateUtils.FORMAT_ABBREV_MONTH;
-
-		// format the date and time
-		// TODO handle sent times when available
-		String mDate = DateUtils.formatDateTime(
-				context,
-				cursor.getLong(cursor
-						.getColumnIndex(MessagesContract.Table.RECEIVED_TIME)),
-				mFlags);
-
-		mTextView = (TextView) view
-				.findViewById(R.id.show_conversation_item_time);
-		mTextView.setText(mDate);
-
-		// add the content of the message
-		mTextView = (TextView) view
-				.findViewById(R.id.show_conversation_item_content);
-		mTextView.setText(cursor.getString(cursor
-				.getColumnIndex(MessagesContract.Table.MESSAGE)));
+		// TextView mTextView;
+		//
+		// // check to see if this is a sent or received message
+		// if (selfPhoneNumber.equals(cursor.getString(cursor
+		// .getColumnIndex(MessagesContract.Table.SENDER_PHONE))) == true) {
+		//
+		// row = inflater.inflate(R.layout.show_conversation_item, parent,
+		// false);
+		//
+		// // this is a message sent by the user
+		// mTextView = (TextView) view
+		// .findViewById(R.id.show_conversation_item_title);
+		// mTextView.setText(selfPhoneNumber);
+		//
+		// populatePhoto(mImageView, cursor, selfPhoneNumber);
+		//
+		// } else {
+		//
+		// // this is a message received by the user
+		// mTextView = (TextView) view
+		// .findViewById(R.id.show_conversation_item_title);
+		// mTextView.setText(cursor.getString(cursor
+		// .getColumnIndex(MessagesContract.Table.RECIPIENT_PHONE)));
+		//
+		// populatePhoto(mImageView, cursor, cursor.getString(cursor
+		// .getColumnIndex(MessagesContract.Table.RECIPIENT_PHONE)));
+		// }
+		//
+		// int mFlags = 0;
+		// mFlags |= DateUtils.FORMAT_SHOW_DATE;
+		// mFlags |= DateUtils.FORMAT_ABBREV_MONTH;
+		//
+		// // format the date and time
+		// // TODO handle sent times when available
+		// String mDate = DateUtils.formatDateTime(
+		// context,
+		// cursor.getLong(cursor
+		// .getColumnIndex(MessagesContract.Table.RECEIVED_TIME)),
+		// mFlags);
+		//
+		// mTextView = (TextView) view
+		// .findViewById(R.id.show_conversation_item_time);
+		// mTextView.setText(mDate);
+		//
+		// // add the content of the message
+		// mTextView = (TextView) view
+		// .findViewById(R.id.show_conversation_item_content);
+		// mTextView.setText(cursor.getString(cursor
+		// .getColumnIndex(MessagesContract.Table.MESSAGE)));
 	}
 
-	private void populatePhoto(ImageView imageView, Cursor cursor, String lookup) {
-
-		// see if this phone number has a contact record associated with it
-		long mContactId = MessageUtils.lookupPhotoId(context, lookup);
-
-		// if a contact record exists, get the photo associated with it
-		// if there is one
-		if (mContactId != -1) {
-
-			Bitmap mPhoto = MessageUtils.loadContactPhoto(context, mContactId);
-
-			// use photo if found else use default image
-			if (mPhoto != null) {
-				imageView.setImageBitmap(mPhoto);
-			} else {
-				imageView.setImageResource(R.drawable.ic_contact_picture);
-			}
-
-		} else {
-			// use the default image
-			imageView.setImageResource(R.drawable.ic_contact_picture_3);
-		}
+	private static class MessageHolder {
+		TextView messageView;
+		TextView timeView;
 	}
 }
