0a5d21d1cc16dfada905d936cb2cd64550cb7bd8
Andrew Bettison <andrew@servalproject.org>
2016-09-12 17:24:57 +0930

    Improve build_setup.sh
    Now passes the NDK_PLATFORM environment variable to the
    libsodium dist-build script, so that libsodium is built with
    the same Android ABI as the app, defined in jni/Application.mk.

--- build_setup.sh
+++ build_setup.sh
@@ -1,16 +1,37 @@
-#!/bin/bash -e
-# Copyright (C) 2016 Serval Project, Inc.
+#!/bin/bash
+# Copyright 2016 Serval Project, Inc.
 #
-# This script will setup your work folder so that everything is ready to build for the first time
+# This script will setup your work folder so that everything is ready to build
+# for the first time.
 #
 # After running this script once, the application can be built with $ ant debug
-#
+
+# Exit on error.
+set -e
+
+# Change to the directory that contains this script.
+case "$0" in
+*/*) cd "${0%/*}";;
+esac
 
 if [ ! -f "${NDK_ROOT}/ndk-build" ]; then
-   echo "Please ensure that NDK_ROOT points to a valid android NDK"
-   echo "See INSTALL.md for more details"
-   exit
+   echo "Please ensure that NDK_ROOT points to a valid android NDK." >&2
+   echo "See INSTALL.md for more details." >&2
+   exit 1
 fi
+
+shopt -s extglob
+
+export NDK_PLATFORM=$(sed -n -e '/^APP_PLATFORM *:\?= */s///p' jni/Application.mk)
+case "$NDK_PLATFORM" in
+android-+([0-9])) ;;
+*)
+   echo "Unsupported NDK target '$NDK_PLATFORM'" >&2
+   echo "Please ensure the jni/Application.mk file is present and correct." >&2
+   echo "See INSTALL.md for more details." >&2
+   exit 1
+   ;;
+esac
 
 git submodule init
 git submodule update
